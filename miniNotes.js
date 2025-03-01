ba3ed5605233149d5314b368dccbcb0c272a504a
Youssef Gabriel Amazirh <74822145+usrmny@users.noreply.github.com>
2025-03-01 02:58:11 -0400

    Update miniNotes.js

--- miniNotes.js
+++ miniNotes.js
@@ -2,14 +2,21 @@ const main = document.querySelector('main')
 function addTask(){
     const task = document.createElement("div")
     const checkbox = document.createElement("INPUT")
-    const title = document.createElement("INPUT")
+    const title = document.createElement("textarea")
+    const trash = document.createElement("i")
     checkbox.setAttribute("type", "checkbox")
-    title.setAttribute("type", "text")
+    trash.className = "fa-solid fa-trash-can"
 
     document.body.insertAdjacentElement('afterbegin', task)
     task.appendChild(checkbox)
     task.appendChild(title)
-    console.log("received")
+    task.appendChild(trash)
+
+    title.setAttribute("class", "titlecss")
+    checkbox.setAttribute("class", "checkboxcss")
+    trash.setAttribute("class", "trashcss") //dissapeared!!!
+    console.log(trash)
+
 }
 
 function openMenu(){}
