d3c1f4af9d5a27d213ddc67c5ad57214314fb928
Youssef Gabriel Amazirh <74822145+usrmny@users.noreply.github.com>
2025-03-02 22:58:04 -0400

    Update miniNotes.js

--- miniNotes.js
+++ miniNotes.js
@@ -1,22 +1,77 @@
-const main = document.querySelector('main')
+const container = document.getElementById('sorted')
+console.log(container.id)
+const textarea = document.querySelectorAll('textarea') //like this?
+textarea.forEach((e) => {
+    e.addEventListener('input', autoScroll)
+})
+
+
 function addTask(){
     const task = document.createElement("div")
     const checkbox = document.createElement("INPUT")
     const title = document.createElement("textarea")
     const trash = document.createElement("i")
     checkbox.setAttribute("type", "checkbox")
-    trash.className = "fa-solid fa-trash-can"
 
-    document.body.insertAdjacentElement('afterbegin', task)
+    container.insertAdjacentElement('afterbegin', task)
     task.appendChild(checkbox)
     task.appendChild(title)
     task.appendChild(trash)
 
+    task.setAttribute("draggable", true)
     title.setAttribute("class", "titlecss")
+    title.setAttribute("id", "unchecked")
     checkbox.setAttribute("class", "checkboxcss")
-    trash.setAttribute("class", "trashcss") //dissapeared!!!
-    console.log(trash)
+    trash.className = "fa-solid fa-trash-can"
 
+    title.addEventListener('input', autoScroll)
+
+    checkbox.addEventListener('click', checked => {
+        if(title.id == "unchecked") {           
+            title.removeAttribute("unchecked")
+            title.setAttribute("id", "checked")
+        }
+        else if(title.id == "checked") {
+            title.removeAttribute("checked")
+            title.setAttribute("id", "unchecked")
+        }
+        //insertBefore
+    })
+    
+    trash.addEventListener('click', removeTask => {
+        task.remove()
+    })
+
+    const allTasks = document.querySelectorAll(".titlecss")
+    allTasks.forEach(task => {
+        task.addEventListener("dragstart", () => {
+            setTimeout(() => task.classList.add("dragging"), 0)
+        })
+        task.addEventListener("dragend", () => {
+            task.classList.remove("dragging")
+        })
+    })
 }
 
+
+
+function autoScroll(task){
+   // const text = document.getElementsByClassName("titlecss")
+    //if(t//lost
+}
+
+
+
+
+
 function openMenu(){}
+
+
+
+
+//move
+//menu => more pages
+//checked task moved down
+//save info
+//mini window for subtasks / notes about task / making the extension
+//autoscroll
