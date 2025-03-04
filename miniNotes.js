5846d8c0ac47c17f46e263f9372011e94952c591
Youssef Gabriel Amazirh <74822145+usrmny@users.noreply.github.com>
2025-03-03 23:17:47 -0400

    Update miniNotes.js

--- miniNotes.js
+++ miniNotes.js
@@ -1,5 +1,4 @@
-const container = document.getElementById('sorted')
-console.log(container.id)
+const box = document.getElementById("list")
 const textarea = document.querySelectorAll('textarea') //like this?
 textarea.forEach((e) => {
     e.addEventListener('input', autoScroll)
@@ -13,36 +12,35 @@ function addTask(){
     const trash = document.createElement("i")
     checkbox.setAttribute("type", "checkbox")
 
-    container.insertAdjacentElement('afterbegin', task)
+    box.appendChild(task)
     task.appendChild(checkbox)
     task.appendChild(title)
     task.appendChild(trash)
 
     task.setAttribute("draggable", true)
-    title.setAttribute("class", "titlecss")
-    title.setAttribute("id", "unchecked")
+    task.setAttribute("class", "task")
+    title.setAttribute("class", "titlecss unchecked")
     checkbox.setAttribute("class", "checkboxcss")
     trash.className = "fa-solid fa-trash-can"
 
     title.addEventListener('input', autoScroll)
 
     checkbox.addEventListener('click', checked => {
-        if(title.id == "unchecked") {           
-            title.removeAttribute("unchecked")
-            title.setAttribute("id", "checked")
+        if(title.classList.contains("unchecked")) {           
+            title.setAttribute("class", "titlecss checked")
+            box.insertAdjacentElement('beforeend', task)
         }
-        else if(title.id == "checked") {
-            title.removeAttribute("checked")
-            title.setAttribute("id", "unchecked")
+        else {
+            title.setAttribute("class", "titlecss unchecked")
+            box.insertAdjacentElement('afterbegin', task)
         }
-        //insertBefore
     })
     
     trash.addEventListener('click', removeTask => {
         task.remove()
     })
 
-    const allTasks = document.querySelectorAll(".titlecss")
+    const allTasks = document.querySelectorAll(".task")
     allTasks.forEach(task => {
         task.addEventListener("dragstart", () => {
             setTimeout(() => task.classList.add("dragging"), 0)
@@ -51,6 +49,21 @@ function addTask(){
             task.classList.remove("dragging")
         })
     })
+
+    const initBox = (e) => {
+        e.preventDefault()
+        const draggingItem = box.querySelector(".dragging")
+        const siblings = [...document.querySelectorAll(".task:not(.dragging)")]
+
+        let nextSibling = siblings.find(sibling => {
+            return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2
+        })
+
+        box.insertBefore(draggingItem, nextSibling)
+    }
+
+    box.addEventListener("dragover", initBox)
+    box.addEventListener("dragenter", e => e.preventDefault())
 }
 
 
@@ -69,9 +82,12 @@ function openMenu(){}
 
 
 
-//move
 //menu => more pages
-//checked task moved down
+
 //save info
 //mini window for subtasks / notes about task / making the extension
 //autoscroll
+
+
+//checked task => not a priority
+//keybind to add notes / move to next/previous note
